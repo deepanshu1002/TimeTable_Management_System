cd0bac70b413f99d858246fbd8b8bee4a0449f56
prathamesh <prathamesh.2309@gmail.com>
2023-08-17 12:13:49 +0530

    feedback entity created

--- spring_boot_backend_template/src/main/java/com/app/entities/Feedback.java
+++ spring_boot_backend_template/src/main/java/com/app/entities/Feedback.java
@@ -3,9 +3,11 @@ package com.app.entities;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -15,22 +17,26 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name="Feedback_tbl")
-@IdClass(FeedbackPkId.class)
+@Table(name = "Feedback_tbl")
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Getter
 @Setter
-public class Feedback{
+public class Feedback {
 	@Id
-	private int studentId;
-	@Id
+	private int feedbackId;
+	@ManyToOne
+	private Users studentId;
+	@ManyToOne
 	private int subjectId;
-	@Id
-	private LocalDate date;
-	private int rating;
-	private String feedback;
+	@ManyToOne
 	private int deptId;
-	
+
+	private LocalDate date;
+	@Column(length = 5)
+	private int rating;
+	@Column(length = 200)
+	private String feedback;
+
 }
