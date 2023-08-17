49246fede01f30d0aabfb53f1edc6932dfcb524a
prathamesh <prathamesh.2309@gmail.com>
2023-08-17 16:51:53 +0530

    department class corrected

--- spring_boot_backend_template/src/main/java/com/app/entities/Department.java
+++ spring_boot_backend_template/src/main/java/com/app/entities/Department.java
@@ -27,22 +27,22 @@ public class Department {
 	private Long deptId;
 	private String deptName;
 	@OneToMany(mappedBy = "dept", cascade = CascadeType.ALL, orphanRemoval = true)
-	private List <Lecture> lectures = new ArrayList<>();
+	private List<Lecture> lectures = new ArrayList<>();
 	@OneToMany(mappedBy = "deptId")
-	private List<Feedback> feedbacks=new ArrayList<Feedback>();
+	private List<Feedback> feedbacks = new ArrayList<Feedback>();
 	@OneToMany(mappedBy = "dept")
-	private List <ClassroomTbl> classroom = new ArrayList<>();
+	private List<ClassroomTbl> classroom = new ArrayList<>();
+	@OneToMany(mappedBy = "dept")
+	private List<Users> user = new ArrayList<>();
 
 	public void addLecture(Lecture l) {
-        lectures.add(l);// dept --> emp
+		lectures.add(l);// dept --> emp
 		l.setDept(this);// emp --> dept
 	}
+
 	public void removeLecture(Lecture l) {
 		lectures.remove(l);
 		l.setDept(null);
 	}
 
-	@OneToMany(mappedBy = "dept")
-	private List <Users> user = new ArrayList<>();
-	
 }
