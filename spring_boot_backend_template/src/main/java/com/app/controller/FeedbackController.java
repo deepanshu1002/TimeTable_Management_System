8014975fb0fd3534db8d02d8f788b38d9e7a73be
prathamesh <prathamesh.2309@gmail.com>
2023-08-17 15:12:26 +0530

    feedback repo added

--- spring_boot_backend_template/src/main/java/com/app/controller/FeedbackController.java
+++ spring_boot_backend_template/src/main/java/com/app/controller/FeedbackController.java
@@ -1,8 +1,15 @@
 package com.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class FeedbackController {
+import com.app.service.FeedbackService;
 
+@RestController
+@RequestMapping("/feedback")
+public class FeedbackController {
+	@Autowired
+	private FeedbackService feedbackService;
+	
 }
