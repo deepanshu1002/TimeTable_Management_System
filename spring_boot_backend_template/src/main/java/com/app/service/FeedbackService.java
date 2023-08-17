faf9721a8a9bdc1dadcae67a500a2792646b383c
prathamesh <prathamesh.2309@gmail.com>
2023-08-17 16:26:14 +0530

    feedback repo added

--- spring_boot_backend_template/src/main/java/com/app/service/FeedbackService.java
+++ spring_boot_backend_template/src/main/java/com/app/service/FeedbackService.java
@@ -1,5 +1,8 @@
 package com.app.service;
 
+import com.app.dto.AddFeedbackReqDTO;
+import com.app.dto.ApiResponseDto;
+
 public interface FeedbackService {
-	
+	ApiResponseDto addFeedback(AddFeedbackReqDTO feedback);
 }
