95d851d3d742e0bfd9b5be7c3081d858ff82d49a
Deepanshu <deepanshuj338@gmail.com>
2023-08-25 08:22:19 +0530

    add cors

--- spring_boot_backend_template/src/main/java/com/app/controller/ClassRoomController.java
+++ spring_boot_backend_template/src/main/java/com/app/controller/ClassRoomController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.app.service.ClassRoomService;
 
 @RestController
 @RequestMapping("/classroom")
+@CrossOrigin(origins = "*")
 public class ClassRoomController {
 	@Autowired
 	private ClassRoomService classRoomService;
--- spring_boot_backend_template/src/main/java/com/app/controller/DepartmentController.java
+++ spring_boot_backend_template/src/main/java/com/app/controller/DepartmentController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.app.service.DepartmentService;
 @RestController
 @RequestMapping("/department")
 @Validated
+@CrossOrigin(origins = "*")
 public class DepartmentController {
 	@Autowired
 	private DepartmentService departmentService;
