a5cbd044cbad35f1ab61438ef63bdaa8e4b44a33
saurav s <sauravsanap000@gmail.com>
2023-08-29 01:01:20 +0530

    isdebar removed

--- TMS_Client/demo/src/App.jsx
+++ TMS_Client/demo/src/App.jsx
@@ -1,16 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
+// import './App.css'
 import { useEffect } from 'react'
 //used to register react-toastify
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-<<<<<<< HEAD
-import{Admin,AddDetails, AdminDashboard} from './components/admin'
 
-=======
-import { Admin, AddDetails } from './components/admin'
->>>>>>> 3c55cd798def85d126df7cc76ea09021897e58a6
+import { Admin, AdminDashboard } from './components/admin'
+
 // import{AddDetails} from './components/admin'
 import RegisterUser from './components/registerUser'
 import ManageUsers from './components/ManageUser'
@@ -32,32 +29,7 @@ import AddLectureData from './components/addLectureData'
 import ManageLeaves from './components/manageLeaves'
 
 import GetLectureDetails from './components/getLectureDetails'
-<<<<<<< HEAD
-//import SideBar from './components/sidebar'
-//import Navbar from './components/navbar'
-import Sidebar from './components/sidebar'
-//import TeacherRatings from './components/teacherRatings'
-import CheckRatings from './components/checkRatings'
-import Rating from './components/newRatings'
 
-function App() {
-
-return (
-    <div className='container-fluid'>
-    
-      <div className='container'>
-        <Routes>
-        <Route path='/' element={<LoginUser/>} />
-         {/* register component */}
-         <Route path='/manageleaves' element={<ManageLeaves/>} />
-        <Route path='/admindashboard' element={<AdminDashboard/>} />
-         <Route path='/register' element={<RegisterUser />} />
-         <Route path='/validuser' element={<ManageUsers />} />  
-         {/*Leave Application component */}
-         <Route path='/leaveapplication' element={<LeaveApplication/>} />
-         {/*GET Leave Application component */}
-         <Route path='/getLeaveApplication' element={<GetLeaveApplication/>} />
-=======
 
 import ForgotPassword from './components/forgotPassword'
 import ForgotEmail from './testmail'
@@ -71,6 +43,8 @@ import NavbarA from './components/navbarA';
 // import Navbar from './components/navbar'
 
 import ProfileEditUser from './components/profileImageEditUser'
+import CheckRatings from './components/checkRatings';
+import Rating from './components/newRatings';
 
 
 
@@ -91,22 +65,22 @@ function App() {
 
     <div className='row'>
       <div className='app-container col-3'>
-      {loginStatus && <NavbarA />}
-      {loginStatus && <Sidebar />}
+        {loginStatus && <NavbarA />}
+        {/* {loginStatus && <Sidebar />} */}
       </div>
       <div className='main-content'>
         <Routes>
           <Route path='/' element={<LoginUser />} />
           {/* register component */}
           <Route path='/manageleaves' element={<ManageLeaves />} />
-          <Route path='/adddetails' element={<AddDetails />} />
+          <Route path='/admindashboard' element={<AdminDashboard />} />
           <Route path='/register' element={<RegisterUser />} />
           <Route path='/validuser' element={<ManageUsers />} />
           {/*Leave Application component */}
           <Route path='/leaveapplication' element={<LeaveApplication />} />
           {/*GET Leave Application component */}
           <Route path='/getLeaveApplication' element={<GetLeaveApplication />} />
->>>>>>> 3c55cd798def85d126df7cc76ea09021897e58a6
+
 
           <Route path='/student' element={<Student />} />
 
@@ -139,36 +113,32 @@ function App() {
 
           <Route path='/editUser' element={<EditUser />} />
 
-         {/* <Route path='/profileEdit' element={<ProfileEditUser/>}/> */}
-         
-<<<<<<< HEAD
-          <Route path='/managedepartment' element={<AddDepartment/>} />
-          
-         <Route path='/manageclassroom' element={<AddClassroom/>} />
+          {/* <Route path='/profileEdit' element={<ProfileEditUser/>}/> */}
 
-         <Route path='/managelab' element={<AddLabVenue/>} />
+          <Route path='/managedepartment' element={<AddDepartment />} />
 
-         <Route path='/managesubject' element={<AddSubjectDetails/>} />
+          <Route path='/manageclassroom' element={<AddClassroom />} />
 
-         <Route path='/timetable' element={<ViewTimetable/>} />
+          <Route path='/managelab' element={<AddLabVenue />} />
 
+          <Route path='/managesubject' element={<AddSubjectDetails />} />
 
-         <Route path='/timetablemetadata' element={<TimeTableMetadata/>} />
+          <Route path='/timetable' element={<ViewTimetable />} />
 
 
-         <Route path='/addlecturedata' element={<AddLectureData/>} />
+          <Route path='/timetablemetadata' element={<TimeTableMetadata />} />
 
-         <Route path='/getlecturedetails' element={<GetLectureDetails/>} />
-         <Route path='/sidebar' element={<Sidebar/>} />
-         <Route path='/ratings' element={<CheckRatings/>} />
-         <Route path="/rating/:subId" element={<Rating/>} />
-         <Route path='/rating' element={<Rating subjectId={1}/>} />
 
-         {/* <Route path='/navbar' element={<Navbar/>} /> */}
-         </Routes>
-=======
+          <Route path='/addlecturedata' element={<AddLectureData />} />
+
+          <Route path='/getlecturedetails' element={<GetLectureDetails />} />
+          <Route path='/sidebar' element={<Sidebar />} />
+          <Route path='/ratings' element={<CheckRatings />} />
+          <Route path="/rating/:subId" element={<Rating />} />
+
+          {/* <Route path='/navbar' element={<Navbar/>} /> */}
         </Routes>
->>>>>>> 3c55cd798def85d126df7cc76ea09021897e58a6
+
       </div>
     </div>
     <ToastContainer />
